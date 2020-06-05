e1c78b9c57bca801459c42e21d1bb672462a02ac
gustavobalbino04 <gubalbino15@gmail.com>
2020-06-05 10:59:15 -0300

    feito

---
+++ Dadosplayer.cs
@@ -0,0 +1,25 @@
+namespace Aula11Polimorfismo
+{
+    public class Dadosplayer
+    {
+        //subre carga 
+        public string Calcular(){
+            return "Dados de jogador:";
+        }
+        public string Calcular(string nick){
+            return $"Nick do jogador : {nick}";
+        }
+        public string Calcular(string nome, string sobrenome){
+            return $"Nome do jodagor:{nome} {sobrenome}";
+        }
+        public string Calcular(string nivel, int fase){
+            return $"Nivel do jogador: {nivel}" + (fase);
+        }
+        public string Calcular(int vida){
+            return $"Vida do jogador:"+vida;
+        }
+        public string Calcular(int vida, int escudo){
+            return $"Vida com escudo:"+(vida+escudo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula11Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dadosplayer player = new Dadosplayer();
+            //sem argumentos
+            System.Console.WriteLine( player.Calcular() );
+            //com 1 argumento em string
+            System.Console.WriteLine( player.Calcular("homen de ferro") );
+            //com 2 argumentos em string
+            System.Console.WriteLine( player.Calcular("Gustavo","Balbino") );
+            //com 1 argumento em string e outro em int
+            System.Console.WriteLine( player.Calcular("intermediario",12) );
+            //com um argumento em int
+            System.Console.WriteLine( player.Calcular(100) );
+            //com 2 argumentos em int
+            System.Console.WriteLine( player.Calcular(100,50) );
+        }
+    }
+}
